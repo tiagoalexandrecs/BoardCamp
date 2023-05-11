aa160a23b9543d2481f27d93d6a9f692b17c9aaf
Tiago <tiagoalex0202@gmail.com>
2023-05-11 20:21:55 -0300

    Agora acho que vai

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/index.routes.js";
+import router from "./Routes/index.routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = 4000;
+const port = 5000;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
