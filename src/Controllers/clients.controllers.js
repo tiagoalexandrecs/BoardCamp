d039c1ebc5381cd3f5b03b3bf902cf469c71f47b
Tiago <tiagoalex0202@gmail.com>
2023-05-12 06:51:57 -0300

    Atualização da tabela

--- src/Controllers/clients.controllers.js
+++ src/Controllers/clients.controllers.js
@@ -2,7 +2,7 @@ import { db } from "../Database/database.connection.js"
 
 export async function getClients(req, res) {
     try {
-      const clients = await db.query("SELECT * FROM clients");
+      const clients = await db.query("SELECT * FROM customers");
       res.send(clients.rows);
     } catch (err) {
       res.status(500).send(err.message);
@@ -11,7 +11,7 @@ export async function getClients(req, res) {
 
 export async function insertClient(req,res){
     const {name, phone, cpf, birthday}= req.body
-    const existing= await db.query(`SELECT * FROM clients WHERE cpf= $1;`, [cpf]);
+    const existing= await db.query(`SELECT * FROM customers WHERE cpf= $1;`, [cpf]);
 
     if(existing){
         return res.sendStatus(409)
@@ -19,7 +19,7 @@ export async function insertClient(req,res){
     
     
     try {
-        await db.query(`INSERT INTO clients (name, phone, cpf, birthday) VALUES
+        await db.query(`INSERT INTO customers (name, phone, cpf, birthday) VALUES
         (${name}, ${phone}, ${cpf}, ${birthday});
     `)
         res.sendStatus(201)
@@ -30,7 +30,7 @@ export async function insertClient(req,res){
 
 export async function getClientById(req, res) {
     const { id } = req.params;
-    try{const client= await db.query(`SELECT * FROM clients WHERE cpf= $1;`, [id]);
+    try{const client= await db.query(`SELECT * FROM customers WHERE cpf= $1;`, [id]);
     if(!client){
         return res.sendStatus(404)
     }
@@ -45,7 +45,7 @@ export async function getClientById(req, res) {
   export async function updateClient(req,res){
     const {id}= req.params
     const {name, phone, cpf, birthday}= req.body
-    const existing= await db.query(`SELECT * FROM clients WHERE cpf= $1;`, [cpf]);
+    const existing= await db.query(`SELECT * FROM customers WHERE cpf= $1;`, [cpf]);
 
     if(existing){
         return res.sendStatus(409)
@@ -53,7 +53,7 @@ export async function getClientById(req, res) {
     
     
     try {
-        await db.query(`UPDATE clients SET name=${name}, phone=${phone}, cpf=${cpf}, birthday=${birthday} WHERE id = $1;
+        await db.query(`UPDATE customers SET name=${name}, phone=${phone}, cpf=${cpf}, birthday=${birthday} WHERE id = $1;
     `, [id])
         return res.sendStatus(201)
     } catch (err) {
