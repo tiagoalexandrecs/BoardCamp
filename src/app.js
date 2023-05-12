2c9301a7fd29772de5e924023e7fd2f22b30a056
Tiago <tiagoalex0202@gmail.com>
2023-05-12 06:31:53 -0300

    Deploy no Render

--- src/app.js
+++ src/app.js
@@ -7,5 +7,5 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-const port = 5000;
+const port = process.env.PORT;
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
