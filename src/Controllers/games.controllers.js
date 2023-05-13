4ef43efecaf5698d83b2b587dab40a61a68643f0
Tiago <tiagoalex0202@gmail.com>
2023-05-13 10:00:43 -0300

    Correction in insertGame

--- src/Controllers/games.controllers.js
+++ src/Controllers/games.controllers.js
@@ -13,7 +13,7 @@ export async function insertGame(req,res){
     const {name, image, stockTotal, pricePerDay}= req.body
     const existing= await db.query(`SELECT * FROM games WHERE name= $1;`,[name]);
 
-    if(existing){
+    if(existing.rows.length !==0){
         return res.sendStatus(409)
     }
     
@@ -22,7 +22,7 @@ export async function insertGame(req,res){
         await db.query(`INSERT INTO games (name, image, "stockTotal" , "pricePerDay") VALUES
         ($1, $2, $3, $4);
     `, [name, image, stockTotal,pricePerDay])
-        res.sendStatus(201)
+        return res.sendStatus(201)
     } catch (err) {
         res.status(500).send(err.message)
     }
